423005d7c2dbb0513f2d711165acd35b91a306bf
nero-tux <neroburner@hotmail.de>
2015-09-08 14:46:01 +0200

    update example-config

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-# separator used by search.py, categories.py, ...
-SEPARATOR = ";"
-import settings
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-LANG            = settings.LANG #"en_US" # can be en_US, fr_FR, ...
-ANDROID_ID      = settings.ANDROID_ID #"38c6523ac43ef9e1"
-# GOOGLE_LOGIN    = 'no-reply@thinkgaming.com'
-GOOGLE_LOGIN    = 'tggreatbritain@thinkgaming.com'
+from __future__ import unicode_literals
 
-GOOGLE_PASSWORD = 'th1nking'
+LANG            = "en_US" # can be en_US, fr_FR, ...
+ANDROID_ID      = None # "38c6523ac43ef9e1"
+GOOGLE_LOGIN    = None # 'someone@gmail.com'
+GOOGLE_PASSWORD = None # 'yourpassword'
 AUTH_TOKEN      = None # "yyyyyyyyy"
 
 # force the user to edit this file
-if any([each == None for each in [ANDROID_ID, GOOGLE_LOGIN, GOOGLE_PASSWORD]]):
+if ANDROID_ID == NONE
+    or all([each is None for each in [GOOGLE_LOGIN, GOOGLE_PASSWORD, AUTH_TOKEN]]):
     raise Exception("config.py not updated")
 
